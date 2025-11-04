a3dabdbcde6d132dd6107d76969a6ce94f060b6a
PratibhaDevi29 <151918953+PratibhaDevi29@users.noreply.github.com>
2025-11-04 16:18:50 +0530

    Update siteMetadata.js
    InstaID

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
   threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  instagram: 'https://www.instagram.com/n0va.in',
   medium: 'https://medium.com',
   bluesky: 'https://bsky.app/',
   locale: 'en-US',

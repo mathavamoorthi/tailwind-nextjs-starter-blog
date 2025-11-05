1b6ccdbd34e0971cb0255cf3b17960fde3d733d6
PratibhaDevi29 <prathibadevi.29@gmail.com>
2025-11-05 15:59:42 +0530

    Update LinkedIn URL in siteMetadata.js to Team Nova company page

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,7 +17,7 @@ const siteMetadata = {
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/company/team-n0va',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com/n0va.in',
   medium: 'https://medium.com',

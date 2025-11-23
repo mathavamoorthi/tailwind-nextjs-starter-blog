79cf23e9fb4a08d0b59de1c6b93fbe3d04e7db74
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-11-23 22:10:35 +0530

    Change type from Authors to Author in Props

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
-import type { Authors } from 'contentlayer/generated'
+import type { Author } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import siteMetadata from '@/data/siteMetadata'
 
 interface Props {
   children: ReactNode
-  content: Omit<Authors, '_id' | '_raw' | 'body'>
+  content: Omit<Author, '_id' | '_raw' | 'body'>
 }
 
 export default function AuthorLayout({ children, content }: Props) {

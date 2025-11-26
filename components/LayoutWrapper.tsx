5f2db8a38e9f9be403e3bccab2333dc02d5771d3
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-11-26 15:55:24 +0530

    Simplify LayoutWrapper by removing Header and Footer
    Removed Header and Footer components, simplifying layout.

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,8 +1,6 @@
 import { Inter } from 'next/font/google'
 import SectionContainer from './SectionContainer'
-import Footer from './Footer'
 import { ReactNode } from 'react'
-import Header from './Header'
 
 interface Props {
   children: ReactNode
@@ -15,17 +13,8 @@ const inter = Inter({
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      {/* Full-height flex column layout */}
-      <div className={`${inter.className} flex min-h-screen flex-col font-sans`}>
-        <Header />
-
-        {/* Content grows to fill vertical space */}
-        <main className="flex-1">
-          {children}
-        </main>
-
-        {/* Footer always at the bottom */}
-        <Footer />
+      <div className={`${inter.className} font-sans`}>
+        {children}
       </div>
     </SectionContainer>
   )

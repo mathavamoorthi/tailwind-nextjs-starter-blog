e6b2e12897504eadb65f49f6942ccb7f4f1969fc
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-11-27 15:53:31 +0530

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/authors', title: 'Authors' },
   { href: '/about', title: 'About' },
 ]
 

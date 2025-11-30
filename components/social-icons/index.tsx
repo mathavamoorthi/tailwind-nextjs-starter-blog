cc511f137399732a24031a310dab70e883c2e225
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-11-30 23:24:36 +0530

    Update index.tsx

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -35,23 +35,23 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
   }
 
   return (
-    <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
-      target="_blank"
-      rel="noopener noreferrer"
-      href={href}
-    >
-      <span className="sr-only">{kind}</span>
+  <a
+    className="text-sm text-gray-500 transition hover:text-gray-600"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}
+  >
+    <span className="sr-only">{kind}</span>
 
-      {lookupKey === 'ctftime' ? (
-        <SocialSvg className="h-6 w-auto max-w-[120px]" />
-      ) : (
-        <SocialSvg
-          className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 h-${size} w-${size}`}
-        />
-      )}
-    </a>
-  )
-}
+    {lookupKey === 'hackthebox' || lookupKey === 'ctftime' ? (
+      <SocialSvg className="!h-auto !w-auto" />
+    ) : (
+      <SocialSvg
+        className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 h-${size} w-${size}`}
+      />
+    )}
+  </a>
+)
+
 
 export default SocialIcon

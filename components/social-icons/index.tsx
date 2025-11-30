80dac21e95808d1008b385edf31372f3eee31933
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-11-30 23:26:33 +0530

    Update index.tsx

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -11,7 +11,7 @@ type KnownKind = keyof typeof components
 
 type SocialIconProps = {
   kind: KnownKind | string
-  href: string | undefined
+  href?: string
   size?: number
 }
 
@@ -34,24 +34,26 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
     return null
   }
 
+  const isWideLogo = lookupKey === 'hackthebox' || lookupKey === 'ctftime'
+
   return (
-  <a
-    className="text-sm text-gray-500 transition hover:text-gray-600"
-    target="_blank"
-    rel="noopener noreferrer"
-    href={href}
-  >
-    <span className="sr-only">{kind}</span>
+    <a
+      className="text-sm text-gray-500 transition hover:text-gray-600"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+    >
+      <span className="sr-only">{kind}</span>
 
-    {lookupKey === 'hackthebox' || lookupKey === 'ctftime' ? (
-      <SocialSvg className="!h-auto !w-auto" />
-    ) : (
-      <SocialSvg
-        className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 h-${size} w-${size}`}
-      />
-    )}
-  </a>
-)
-
+      {isWideLogo ? (
+        <SocialSvg className="!h-auto !w-auto" />
+      ) : (
+        <SocialSvg
+          className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 h-${size} w-${size}`}
+        />
+      )}
+    </a>
+  )
+}
 
 export default SocialIcon

7f4b176ec608ec153ef95285d16767401aeca456
mathavamoorthi <152806651+mathavamoorthi@users.noreply.github.com>
2025-12-05 23:20:51 +0530

    Update PostLayout.tsx

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -58,8 +58,8 @@ export default function PostLayout({
             </div>
           </header>
 
-          {/* Grid: col1 author (narrow), col2 content (wide), col3 TOC (medium) */}
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-[240px,minmax(0,2.3fr),minmax(0,1.4fr)] xl:gap-x-8 xl:divide-y-0 dark:divide-gray-700">
+          {/* Back to simple, aligned 4-column grid */}
+          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0 dark:divide-gray-700">
             {/* Author sidebar (left) */}
             <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Author</dt>
@@ -99,8 +99,8 @@ export default function PostLayout({
               </dd>
             </dl>
 
-            {/* Main content (center, wider column) */}
-            <div className="divide-y divide-gray-200 xl:col-start-2 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
+            {/* Main content (center, spans col 2–3) */}
+            <div className="divide-y divide-gray-200 xl:col-span-2 xl:col-start-2 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
               <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
 
               {/* Read next section */}
@@ -141,7 +141,7 @@ export default function PostLayout({
 
             {/* TOC (right, sticky) */}
             {toc?.length > 0 && (
-              <aside className="hidden xl:block xl:pt-11 xl:col-start-3 xl:row-span-2">
+              <aside className="hidden xl:block xl:pt-11 xl:col-start-4 xl:row-span-2">
                 <NovaTOC toc={toc} />
               </aside>
             )}
